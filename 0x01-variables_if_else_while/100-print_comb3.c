93ab411873249527d52e4d1ff71a816ffb12b4d4
Moshi <favour09@connect.hku.hk>
2022-10-14 15:53:57 +0000

    C task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of 2 digits
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for(num1 = 0; num1 < 10; num1++)
+	{
+		for (num2=0; num2 < 10; num2++)
+		{
+			if(num2 > num1)
+			{
+				putchar(num1);
+				putchar(num2);
+				if (num1 < 8)
+				{
+					putchar(',');
+					putchar(' ')
+				}
+			}
+		}
+	}
+	return (0);
+}
