208f3b23a0287aa89518527a8bab66ca7f0a2c80
Moshi <favour09@connect.hku.hk>
2022-10-14 15:21:08 +0000

    C task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints base 16 numbers
+ * Retur: 0 if successful
+ */
+int main(void)
+{
+	char letter;
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		putchar(digit + '0');
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
