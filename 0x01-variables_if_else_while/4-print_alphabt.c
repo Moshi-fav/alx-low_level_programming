7435051672c88c2647875ea2398013766f37cafa
Moshi <favour09@connect.hku.hk>
2022-10-14 15:01:16 +0000

    C task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase except q and e
+ * Return: o if successful
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <='z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
