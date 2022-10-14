ae453a1939212cb24ea64b0b4279ee9dab925b0f
Moshi <favour09@connect.hku.hk>
2022-10-14 15:57:44 +0000

    C task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,11 @@ int main(void)
 	int num1;
 	int num2;
 
-	for(num1 = 0; num1 < 10; num1++)
+	for (num1 = 0; num1 < 10; num1++)
 	{
 		for (num2 = 0; num2 < 10; num2++)
 		{
-			if(num2 > num1)
+			if (num2 > num1)
 			{
 				putchar(num1 + '0');
 				putchar(num2 + '0');
