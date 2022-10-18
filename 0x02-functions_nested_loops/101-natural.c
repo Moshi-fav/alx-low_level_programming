151bbc3de17dac44e3b799ef569a3d5d0cecfe96
Moshi <favour09@connect.hku.hk>
2022-10-18 23:52:12 +0000

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints sum of multiples of 3 or 5 below 1024
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i, sum;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 5 == 0 || i % 3 == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+}
