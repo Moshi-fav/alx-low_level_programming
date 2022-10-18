d1d3e99e8da502f392a1cb6c04c957acc66e8da7
Moshi <favour09@connect.hku.hk>
2022-10-18 15:43:09 +0000

    task 8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit -> prints last digit of argument
+ * @n: integer parameter
+ * Return: last digit of argument
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
