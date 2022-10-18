ddf43b683faf840e968ff97da37b2e679b003f1b
Moshi <favour09@connect.hku.hk>
2022-10-18 22:34:29 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add -> adds 2 integers and returns result
+ * @a: int parameter to be added
+ * @b: int parameter to be added
+ * Return: a+b if successful
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
