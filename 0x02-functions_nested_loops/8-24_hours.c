d8760e63480ec0a2e6bb6a050f97058e5809b0a1
Moshi <favour09@connect.hku.hk>
2022-10-18 16:05:32 +0000

    task 5 fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,14 +6,15 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 	
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			for (c = 0; c <= 9; c++)
+			for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
 				{
+					if(a < 2 || (a == 2 && b < 4))
 					_putchar(a + '0');
 					_putchar(b + '0');
 					_putchar(':');
