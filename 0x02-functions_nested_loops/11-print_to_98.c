06dbe1415b26af0f23ad4676992d090f0e3e7b1d
Moshi <favour09@connect.hku.hk>
2022-10-18 23:15:47 +0000

    task 11 again

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,38 +1,24 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * print_to_98 -> prints all digits from a give number to 98
- * @n: number to start printing from
+ * print_to_98 -> prints from given nukmber to 98
+ * @n: number to print from
  */
 void print_to_98(int n)
 {
-	int i, j;
+	int i;
 
-	i = n / 10;
-	j = n % 10;
-
-	for (i = i; i <= 9; i++)
+	printf("%d, ", n);
+	if (n > 98)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			if ((i < 9 || (i == 9 && j <= 8)) &&
-					(i > n / 10 || (i == n / 10 && j >= n % 10)))
-			{
-				if (i == 0)
-				{
-					if (j != 0)
-						_putchar(' ');
-					_putchar(j + '0');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(i + '0');
-					_putchar(j + '0');
-				}
-				if (i != 9 || j != 8)
-					_putchar(',');
-			}
-		}
+		for (n = n - 1; n > 98; n--)
+			printf("%d, ", n);
 	}
+	else
+	{
+		for (n = n + 1; n < 98; n++)
+			printf("%d, ", n);
+	}
+	printf("%d", 98);
 }
