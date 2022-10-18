912d3370fad4c02f000c54fdabc1f7d95d4b88a7
Moshi <favour09@connect.hku.hk>
2022-10-18 15:12:38 +0000

    task 2 fic

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@ void print_alphabet(void)
 	int i;
 	int letter;
 
-	for (int i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (int letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		putchar('\n');
 	}
