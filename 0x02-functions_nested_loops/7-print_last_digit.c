d22ecd0ca8ed9cf516359d509ddef0e51383ae42
Moshi <favour09@connect.hku.hk>
2022-10-18 15:54:15 +0000

    task 8 fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,11 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		_putchar((-n % 10) + '0');
-		return (-n % 10);
+		int l = -n % 10;
+		if (l < 0)
+			l = -l;
+		_putchar(l + '0');
+		return (l);
 	}
 	else
 	{
