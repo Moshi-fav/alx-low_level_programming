d2e853d466bc621ae7f90ddf9ac09c96c22de206
Moshi <favour09@connect.hku.hk>
2022-10-18 14:56:21 +0000

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints the alpahbet in lowercase
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char letter;
+	for(letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+	return (0);
+}
