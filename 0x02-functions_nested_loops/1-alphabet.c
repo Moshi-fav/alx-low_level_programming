19c007bc707d05bae670d769002c4af3cfe84f57
Moshi <favour09@connect.hku.hk>
2022-10-18 15:04:38 +0000

    2fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * main - prints the alpahbet in lowercase
+ * print_alphabet -> prints the alpahbet in lowercase
  * Return: 0 if successful
  */
 void print_alphabet(void)
 {
 	int letter;
-	for(letter = 'a'; letter <= 'z'; letter++)
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n');
 }
