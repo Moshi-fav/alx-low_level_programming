2868b447a30603eca31b1c95ab07963682e5f8d7
Moshi <favour09@connect.hku.hk>
2022-10-19 00:27:44 +0000

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include < stdio.h>
+/**
+ * main - prints forst 50 fibonacci numbers
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i = 1, j = 2, k, count = 2;
+
+	printf("%d, %d, ", i, j);
+	for (; count <= 49; count++)
+	{
+		k = i + j;
+		printf("%d, ", k);
+		j = k;
+		i = j;
+	}
+	k = i + j;
+	printf("%d\n", k);
+	return (0);
+}
