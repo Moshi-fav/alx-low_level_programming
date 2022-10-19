5ba28d6bd23ac0c8de5d40ba360f9610d8961c4c
Moshi <favour09@connect.hku.hk>
2022-10-19 00:33:41 +0000

    task 14 fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,7 +8,7 @@ int main(void)
 	long int i = 1, j = 2, k, count = 2;
 
 	printf("%ld, %ld, ", i, j);
-	for (; count <= 49; count++)
+	for (; count <= 48; count++)
 	{
 		k = i + j;
 		printf("%ld, ", k);
