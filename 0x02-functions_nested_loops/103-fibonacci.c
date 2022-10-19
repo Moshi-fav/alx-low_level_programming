af80dd3e438187309f14241a0e53b206fafb1623
Moshi <favour09@connect.hku.hk>
2022-10-19 00:46:29 +0000

    task 15 fix

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,10 +7,10 @@ int main(void)
 {
 	unsigned long int i = 1, j = 2, k, sum = 2;
 
-	while (k < 4000000)
+	while (k <= 4000000)
 	{
 		k = i + j;
-		if (k % 2 == 0)
+		if ((k % 2) == 0)
 			sum = sum + k;
 		i = j;
 		j = k;
