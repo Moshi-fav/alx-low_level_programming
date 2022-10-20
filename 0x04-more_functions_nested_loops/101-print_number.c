2910bcce3c4ccb7570a373111b4f87ce76f6b7c4
Moshi <favour09@connect.hku.hk>
2022-10-20 15:56:09 +0000

    task 13

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <math.h>
+/**
+ * print_number -> prints a number
+ * @n: number to print
+ */
+void print_number(int n)
+{
+	unsigned int i, j = 1, count;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	while (n)
+	{
+		count = 0;
+		while (j > 10)
+		{
+			j = n / 10;
+			count = count + 1;
+		}
+		_putchar(j + '0');
+		n = n - (j * pow(10, count));
+	}
+	_putchar('\n');
+
+}
