3eabf21fef52f4dd06d63d134fe0a66eb7bd5011
Moshi <favour09@connect.hku.hk>
2022-10-20 13:57:05 +0000

    task 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers -> prints digits form 0 to 9 followed by a newline
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
