71b19cb5403a2373faf15ebd5057ed1519ac254b
Moshi <favour09@connect.hku.hk>
2022-10-20 14:27:41 +0000

    task7

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line -> printa a line in the terminal
+ * @n: number of times to print _
+ */
+void print_line(int n)
+{
+	if (n >= 1)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
