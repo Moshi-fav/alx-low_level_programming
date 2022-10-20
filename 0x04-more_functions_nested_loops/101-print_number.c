ffc858c35d59dd38c1c313b51ba6f64da9b259eb
Moshi <favour09@connect.hku.hk>
2022-10-20 23:46:43 +0000

    task 13 fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,45 +1,16 @@
 #include "main.h"
 /**
- * print_number -> prints a number
- * @n: number to print
+ * print_number -> prints a number using _putchar()
+ *@n: number to print
  */
 void print_number(int n)
 {
-	unsigned int num, j, mult;
-	int count, i;
-
 	if (n < 0)
 	{
-		num = -n;
 		_putchar('-');
+		n = -n;
 	}
-	else
-		num = n;
-
-	while (num)
-	{
-		if (num < 9)
-		{
-			_putchar(num + '0');
-			break;
-		}
-		else
-		{
-			j = 11;
-			count = 0;
-			while (j >= 10)
-			{
-				j = num / 10;
-				count = count + 1;
-			}
-			_putchar(j + '0');
-			mult = 1;
-			for (i = 0; i < count; i++)
-				mult = mult * 10;
-
-			num = num - (j * mult);
-		}
-	}
-	_putchar('\n');
-
+	if (n / 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
 }
---
+++ 0x04-more_functions_nested_loops/101-print_number_a.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * print_number -> prints a number
+ * @n: number to print
+ */
+void print_number(int n)
+{
+	int num, j, mult;
+	int count, i;
+
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	}
+	else
+		num = n;
+
+	while (num >= 1)
+	{
+		if (num < 9)
+		{
+			_putchar(num + '0');
+			break;
+		}
+		else
+		{
+			j = 11;
+			count = 0;
+			while (j >= 10)
+			{
+				j = num / 10;
+				count = count + 1;
+			}
+			_putchar(j + '0');
+			mult = 1;
+			for (i = 0; i < count; i++)
+				mult = mult * 10;
+
+			num = num - (j * mult);
+		}
+	}
+	_putchar('\n');
+
+}
