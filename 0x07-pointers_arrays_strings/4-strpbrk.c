9133cc82ab69693b92efd74a906318e0173d5206
Moshi <favour09@connect.hku.hk>
2022-10-31 09:29:36 +0000

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any char from another string
+ * @s: pointer to string to search
+ * @accpet: pointer to bytes to search for
+ * Return: pointer to byte in s or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j] && s[i])
+				return s[i];
+		}
+	}
+	return (NULL);
+}
