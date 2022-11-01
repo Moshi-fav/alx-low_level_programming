a91b2099ccdb889b936dceb75a81d43e87bb8239
Moshi <favour09@connect.hku.hk>
2022-11-01 01:54:10 +0000

    task 7 fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,12 +7,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum = 0, sum2 = 0;
+	int i, sum = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum += a[i][i];
-		sum2 += a[i][size - i - 1];
+		sum += a[(size + 1) * i];
+		sum2 += a[(size - 1) * (b + 1)];
 	}
 	printf("%d, %d\n", sum, sum2);
 }
