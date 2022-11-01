babc8e0c33d0e456ca94d60fbcfaf2615a7a63b7
Moshi <favour09@connect.hku.hk>
2022-11-01 02:00:39 +0000

    5 fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j]; j++)
 		{
-			if (*(haystack + i + j) != *(needle + j)
+			if (*(haystack + i + j) != *(needle + j))
 				break;
 			if (*(needle + j + 1) == '\0')
 				return (haystack + i);
