7b29032d6cf88860399da5c74b1a495768c1cfa6
Moshi <favour09@connect.hku.hk>
2022-11-02 15:22:09 +0000

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _pow_recursion - raises a given number to a power of another given number
+ * @x: base
+ * @y: power
+ * Return: x raised to power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
