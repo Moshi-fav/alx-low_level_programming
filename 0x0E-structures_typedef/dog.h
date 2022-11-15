13823b380b196f4ffc2e8d7e4f64a7353840e5bb
Moshi <favour09@connect.hku.hk>
2022-11-15 02:39:14 +0000

    intial commit and task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+#include <stdlib.h>
+/**
+ * struct dog - new struct for storing dog variables
+ * @name: string pointer for name
+ * @age: float age
+ * @owner: string pointer storing owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
+
