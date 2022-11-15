54df7dd2d1e58be64a21b0673dadd7732b00727a
Moshi <favour09@connect.hku.hk>
2022-11-15 02:54:00 +0000

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * print_dog - prints elements of dog struct
+ * @d: struct to print
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+	printf("Age: %f\n", d->age);
+	if (d->owner)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
+}
